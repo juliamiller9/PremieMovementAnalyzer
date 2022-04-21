8e54c45104a6ea08d48299bfd32de884c19a2234
juliamiller9 <juliam@wustl.edu>
2022-04-21 16:15:52 -0500

    smoothing filter, custom file name, fixed G->g

--- accelV2.ino
+++ accelV2.ino
@@ -22,14 +22,11 @@ BLEService accelService("2eabb710-354b-4066-95af-d8c1d4723e48"); // Bluetooth® 
 // Bluetooth® Low Energy LED Switch Characteristic - custom 128-bit UUID, read and writable by central
 BLEByteCharacteristic acceleration("61811ad4-035a-4d1f-a60d-39828b887606", BLERead | BLEWrite | BLENotify);
 BLEByteCharacteristic count("f681bdcf-33af-4278-acc5-46770b2a331d", BLERead | BLEWrite | BLENotify);
-float threshold = 1.2; //G's
-float timeThreshold = 500;
+float threshold = 1.15; //g's
+float timeThreshold = 500; //miliseconds
 unsigned long timeOfCrossThreshold = 0;
 float steps = 0.0;
-float avg_x;
-float avg_y;
-float avg_z;
-
+float sampleSize = 20;
 float values[2];
 
 #include <Arduino_LSM9DS1.h>
@@ -58,10 +55,6 @@ void setup() {
   // add service
   BLE.addService(accelService);
 
-  // set the initial value for the characeristic:
-  //count.writeValue(0);
-  //acceleration.writeValue(0);
-
   // start advertising
   BLE.advertise();
 }
@@ -84,24 +77,23 @@ void loop() {
     while (central.connected()) {
       float x, y, z;
       if (IMU.accelerationAvailable()) {
-        IMU.readAcceleration(x, y, z);
-        x = x-avg_x;
-        y=y-avg_y;
-        z = z-avg_z;
-        float magnitude = sqrt(x*x+y*y+z*z); 
-
-      if (magnitude > threshold) {
+        float reading = 0;
+        for (int i=0; i < sampleSize; i++) {
+          IMU.readAcceleration(x, y, z);
+          reading += sqrt(x*x+y*y+z*z);
+        }
+      float magnitude = reading / sampleSize;
+      if (magnitude > threshold && !flag) {
         if (millis()-timeOfCrossThreshold > timeThreshold) {
           
           Serial.println("Movement Detected!");
           timeOfCrossThreshold = millis();
           flag = true;
+          steps = steps + 1;
         }     
       }
-      else if (flag = true) {
-          steps=steps+1;
+      else if (flag) {
           flag=false;
-        }
       }
       values[0] = magnitude;
       values[1] = steps;
@@ -109,21 +101,11 @@ void loop() {
         count.writeValue(steps);
         acceleration.writeValue((int)(magnitude * 100)); //Going to be multiplying by 100 so can send as an int
       }
-      Serial.print(values[0]);
-      Serial.print('\t');
-      Serial.println(values[1]);
-      //acceleration.writeValue(magnitude);
-       
-//      count.readValue(stepPrint);
-//      acceleration.readValue(magnitudePrint);
-//      Serial.print(stepPrint);
-//      Serial.print('\t');
-//      Serial.println(magnitudePrint);
     }
    }
     // when the central disconnects, print it out:
     Serial.print(F("Disconnected from central: "));
     Serial.println(central.address());
-    
+
   }
 }
